73b8c5a9bcc731e858c34b9ac524e92943e6ba33
Xiangyue Cai <xiangyue.cai@liferay.com>
2018-12-06 16:58:37 +0800

    Update standup.py

--- health/standup.py
+++ health/standup.py
@@ -2,8 +2,8 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 import time
-sender = '920836822@qq.com'
-to = '920836822@qq.com'
+sender = 'XXX@XX.com'
+to = 'XXX@XX.com'
 msg = MIMEText('为了您的健康,起来活动活动,喝点儿水!!!啦啦啦啦啦啦啦啦啦.....此程序每隔一个小时会自动给您发送一封邮件','plain','utf-8')
 msg['subject'] = '来自专属python程序的亲切问候^_^'
 msg['from'] = formataddr(['发件人昵称',sender])
@@ -11,7 +11,7 @@ msg['to']   = formataddr(['收件人昵称',to])
 passcode = ''
 
 def send_email():
-    server = smtplib.SMTP_SSL('smtp.qq.com',465)
+    server = smtplib.SMTP_SSL('smtp.xx.com',465)
     server.set_debuglevel(1)
     server.login(sender,passcode)
     server.sendmail(sender,[sender, to], msg.as_string())
